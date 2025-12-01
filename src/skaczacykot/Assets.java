6b3086350d2b4f58de750ff364db1cbdb8ea3916
Bartlomiej Baranski <baranskibartek@interia.pl>
2025-12-01 22:50:42 +0100

    add file to resources - images loading

---
+++ src/skaczacykot/Assets.java
@@ -0,0 +1,25 @@
+package skaczacykot;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.net.URL;
+
+final class Assets {
+    private Assets() {}
+
+    static BufferedImage img(String name) {
+        try {
+            // dopasowane do /resources/images/<plik> w JAR
+            String full = "/resources/images/" + name;
+            URL url = Assets.class.getResource(full);
+            if (url == null) {
+                System.err.println("Nie znaleziono zasobu: " + full);
+                return null;
+            }
+            return ImageIO.read(url);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
